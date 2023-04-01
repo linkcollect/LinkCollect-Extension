b1ac356aa768955e62d1121425c5cd3638844c4a
subhradwi01 <subhradwi01@gmail.com>
2023-04-01 22:05:08 +0530

    Home new created

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,17 +1,11 @@
 import React from 'react'
-import logo from "../assets/Logo.svg"
+
 const Home = () => {
   return (
-    <div className='p-[2rem] flex flex-col justify-center items-center h-[80%]'>
-        <img className='w-[150px]' src={logo}/>
-        <div className='conatiner text-center'>
-            <p className='text-[18px] font-medium mb-2 mt-2'>More than a bookmarking tool</p>
-            <p className='text-lightText text-[14px] flex flex-col'>
-                <span>Save from anywhere</span>
-                <span>Share to anyone</span>
-                <span>Import anyone</span>
-            </p>
-            <button className='py-[10px] px-[36px] bg-primary text-[14px] font-medium mt-3 rounded-md'>Get Started</button>
+    <div>
+        <div className='flex justify-between'>
+            <p>Collection list<span>8</span></p>
+            <button className=''></button>
         </div>
     </div>
   )
