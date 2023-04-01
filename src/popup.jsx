6f13088bc448b6ccb7fd6b6564ffeb947ea1168f
subhradwi01 <subhradwi01@gmail.com>
2023-04-02 00:03:33 +0530

    popup js screens added

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./popup.css"
-const Popup = ()=>{
-    return <div className="text-5xl">LinkCollect</div>
-}
+import "./popup.css";
+import Splash from "./Screens/Splash";
+import Layout from "./Layout/Layout";
+import Login from "./Screens/Login";
+import Home from "./Screens/Home";
+const Popup = () => {
+  return (
+    <>
+      <Layout>
+        
+        {/* <Splash /> */}
+        {/* <Login/> */}
+        <Home/>
+      </Layout>
+    </>
+  );
+};
 
 const rootElement = document.getElementById("linkcollect-target");
 const root = createRoot(rootElement);
 
-
 root.render(
-    <StrictMode>
-      <Popup />
-    </StrictMode>
-  );
-  
+  <StrictMode>
+    <Popup />
+  </StrictMode>
+);
