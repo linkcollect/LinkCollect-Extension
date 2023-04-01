e8684a2b3a0c0e9961937441f1b967a2b271339c
subhradwi01 <subhradwi01@gmail.com>
2023-04-01 21:51:44 +0530

    Text update

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -6,8 +6,12 @@ const Home = () => {
         <img className='w-[150px]' src={logo}/>
         <div className='conatiner text-center'>
             <p className='text-[18px] font-medium mb-2 mt-2'>More than a bookmarking tool</p>
-            <p className='text-lightText'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. </p>
-            <button className='py-[10px] px-[36px] bg-primary text-[14px] font-medium mt-2 rounded-md'>Get Started</button>
+            <p className='text-lightText text-[14px] flex flex-col'>
+                <span>Save from anywhere</span>
+                <span>Share to anyone</span>
+                <span>Import anyone</span>
+            </p>
+            <button className='py-[10px] px-[36px] bg-primary text-[14px] font-medium mt-3 rounded-md'>Get Started</button>
         </div>
     </div>
   )
