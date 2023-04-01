23f54bf70bb3b3129edb343ab9398c07b177021f
subhradwi01 <subhradwi01@gmail.com>
2023-04-01 19:45:09 +0530

    Logo comilation added to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,53 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 const autoprefixer = require("autoprefixer");
 const tailwindcss = require("tailwindcss");
 module.exports = {
-    entry:{
-        popup:"./src/popup.jsx",
-        background:"./src/background/background.js",
-        contentScript: "./src/background/background.js"
-    },
-    output:{
-        path: path.resolve(__dirname,'dist'),
-        filename:"[name].js"
-    },
-    module: {
-        rules: [{ 
-            test: /\.(js|jsx)$/, 
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options : {
-                    presets:['@babel/preset-env','@babel/preset-react']
-                }
-            }
-        },{
-            use:['style-loader','css-loader',
-            {
-                loader:'postcss-loader',
-                options:{
-                    postcssOptions:{
-                        ident:'postcss',
-                        plugins:[tailwindcss,autoprefixer]
-                    }
-                }
-            }
-            ],
-            test:/\.css$/i,
-        }],
-    },
-    plugins: [new HtmlWebpackPlugin({ 
-        template: './src/popup.html' ,
-        filename: "popup.html"
+  entry: {
+    popup: "./src/popup.jsx",
+    background: "./src/background/background.js",
+    contentScript: "./src/background/background.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                ident: "postcss",
+                plugins: [tailwindcss, autoprefixer],
+              },
+            },
+          },
+        ],
+        test: /\.css$/i,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/popup.html",
+      filename: "popup.html",
     }),
-    new CopyPlugin({patterns:[
-        { from: 'public'}
-        
-    ]}),
-],
-}
+    new CopyPlugin({ patterns: [{ from: "public" },{from:path.resolve("./src/assets")}] }),
+  ],
+};
