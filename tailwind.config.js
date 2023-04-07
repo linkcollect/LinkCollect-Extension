6e0361d0f70b00b7f30de2e591ee88f930c23b3b
subhradwi01 <subhradwi01@gmail.com>
2023-04-07 11:14:39 +0530

    New theme setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,17 @@ module.exports = {
       blackbgOne:"#0D131D",
       blackbgTwo: "#141C27",
       darkgrey:"#202427",
-      lightText:"#9CA3AF"
+      lightText:"#9CA3AF",
+
+      bgPrimary:"#fffff",
+      bgSecodary:"#f9f9f9",
+      bgGrey:"#f1f1f1",
+      primary:"#6166f1",
+      secodary: "#dedff1",
+      textPrimary:"#232438",
+      textSecondary:"#636472",
+      success:"#62b560"
+
     }
   },
   plugins: [],
