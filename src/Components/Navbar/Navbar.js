05db3f80524d334cc1c356202cd7a6cceed76349
subhradwi01 <subhradwi01@gmail.com>
2023-04-07 14:29:59 +0530

    Navbar fixed

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,66 +1,25 @@
 import React, { useState } from "react";
-import logo from "../../assets/Logo.svg";
-import linkArrow from "../../assets/linkArrow.svg";
+import logo from "../../assets/Branding/logo2.svg";
 import avatar from "../../assets/avatar.svg";
-import angalerArrow from "../../assets/angleArrow.svg";
-import command from "../../assets/command.svg";
-import openLink from "../../assets/openLink.svg"
-import help from "../../assets/help.svg"
-import logoout from "../../assets/logout.svg"
+import SideMenu from "../SideMenu/SideMenu";
+
 
 const Navbar = () => {
-  const [isLoggedIn, setIsloggedIn] = useState(true);
-  const [show,setShow]=useState(false);
+  const [show, setShow] = useState(false);
   return (
-    <div className="flex justify-between h-[50px] px-5 items-center pt-3">
+    <div className="flex justify-between h-[80px] items-center px-3 shadow-md">
       <div>
-        <img src={logo} className="w-[30px]" />
+        <img src={logo} className="w-[40%]" />
       </div>
-      {!isLoggedIn && (
-        <div className="flex text-lightText items-center text-[15px]">
-          <a href="" className="mr-[5px]">
-            vist linkcollect.io
-          </a>
-          <img className="w-[10px]" src={linkArrow} />
-        </div>
-      )}
-      {isLoggedIn && (
-        <>
-          <p className="text-xl font-medium">Home</p>
-          <div className="relative">
-            <div className="bg-darkgrey flex items-center justify-between px-1 rounded-full w-[60px] h-[30px] relative" >
-              <div className="w-[25px] rounded-full overflow-hidden">
-                <img src={avatar} />
-              </div>
-              <div className="rotate-[-90deg] pb-2 cursor-pointer" onClick={()=>setShow(preState=>!preState)}>
-                <img src={angalerArrow} />
-              </div>
-            </div>
-            {show&& <div className="absolute z-[99] top-[35px] right-[4px]">
-              <div className="w-[12rem] rounded-md bg-darkgrey px-3 pt-2">
-                <ul className="text-[16px]">
-                  <li className="flex justify-between pb-2">
-                    <p>Open in Web</p>
-                    <button><img src={openLink}/></button>
-                  </li>
-                  <li className="flex justify-between pb-2">
-                    <p>View Commands</p>
-                    <button><img src={command}/></button>
-                  </li>
-                  <li className="flex justify-between pb-2">
-                    <p>Help</p>
-                    <button><img src={help}/></button>
-                  </li>
-                  <li className="flex justify-between pb-2">
-                    <p>Logout</p>
-                    <button><img src={logoout}/></button>
-                  </li>
-                </ul>
-              </div>
-            </div>}
+
+      <div className="">
+          <div className="w-[40px] rounded-full overflow-hidden cursor-pointer" onClick={()=>setShow(prevState=>!prevState)}>
+            <img src={avatar} />
           </div>
-        </>
-      )}
+      </div>
+      {show && (
+          <SideMenu/>
+        )}
     </div>
   );
 };
---
+++ src/Components/SideMenu/SideMenu.js
@@ -0,0 +1,54 @@
+import React from "react";
+import command from "../../assets/Icons/command.svg";
+import openLink from "../../assets/Icons/openLink.svg";
+import help from "../../assets/Icons/help.svg";
+import logoout from "../../assets/Icons/logout.svg";
+
+
+const MenuItem =  ({name,icon,onCLickHandler}) =>{
+    return (
+        <li className="flex items-center pb-2 h-[50px] pl-3 border-b border-secodary">
+            <button>
+              <img src={icon} className="w-[17px]" />
+            </button>
+            <p className="text-textPrimary pl-2 text-xl font-light">{name}</p>
+        </li>
+    )
+}
+
+
+const SideMenu = () => {
+
+    const menuList =  [{
+        name:"Open in Web",
+        icon: openLink,
+    },
+    {
+        name:"View Commands",
+        icon: command,
+    },
+    {
+        name:"Help",
+        icon:help
+    },
+    {
+        name:"Logout",
+        icon:logoout
+    }
+
+]
+
+  return (
+    <div className="fixed z-[99] overflow-hidden h-full top-[81px] right-0">
+      <div className="w-[15rem] h-full bg-bgPrimary pt-2">
+        <ul className="text-[16px]">
+          {menuList.map(menu=><MenuItem name={menu.name} icon={menu.icon}/>)}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+
+
+export default SideMenu;
