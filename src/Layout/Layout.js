89f9b860a88bb0c4a6c7b3e1cef2c7f1717b72aa
subhradwi01 <subhradwi01@gmail.com>
2023-04-07 14:58:53 +0530

    Layout update

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -2,9 +2,10 @@ import React from "react";
 import Navbar from "../Components/Navbar/Navbar";
 const Layout = ({ children }) => {
   return (
-    <div className="bg-blackbg w-[400px] h-[600px] rounded-md">
+    <div className="bg-bgPrimary w-[400px] h-[600px] rounded-md">
       <Navbar />
       {children}
+      <div className="fixed bottom-[20px] left-0 text-primary text-light w-full text-center ">Powered by Linkcollect.io</div>
     </div>
   );
 };
