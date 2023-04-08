117644396ab5c0e919f5ff29435a9474a20c6039
subhradwi01 <subhradwi01@gmail.com>
2023-04-08 23:35:58 +0530

    navigator for new collection added

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import addIcon from "../assets/Icons/add-tab.svg";
 import CollectionItem from "../Components/CollectiionItem/CollectionItem";
 import noCollectionItem from "../assets/Icons/no-collection.svg";
 import SearchBox from "../Components/SearchBox/SearchBox";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+// import { getAllCollectionsWithoutTimelines } from "../api/collectionService";
 
-const EmptyCollections = () => {
+const EmptyCollections = ({navigator}) => {
   return (
     <div className="flex h-[70%] flex-col justify-around items-center">
-      <button className="py-[10px] px-[9px] w-[200px] flex justify-center bg-primary text-[17px] font-lg font-bold rounded-md flex items-center">
+      <button onClick={navigator} className="py-[10px] px-[9px] w-[200px] flex justify-center bg-primary text-[17px] font-lg font-bold rounded-md flex items-center">
         {" "}
         <img src={addIcon} className="mr-2" /> Create Collection{" "}
       </button>
@@ -23,9 +26,23 @@ const EmptyCollections = () => {
 
 const Home = () => {
   const [empty, setEmpty] = useState(false);
+  const [collections,SetCollections] = useState([]);
+  const auth = useSelector(state=>state.auth);
+  const navigate = useNavigate();
+  const createCollectionRedicector =  () => {
+    navigate("/new-collection");
+  }
 
-  if (empty) {
-    return <EmptyCollections />;
+  useEffect(()=>{
+    if(!auth.token){
+      return navigate("/")
+    }
+    // const getCollections = async () =>{
+    //   const data = await getAllCollectionsWithoutTimelines()
+    // }
+  },[])
+  if (collections.length===0) {
+    return <EmptyCollections navigator={createCollectionRedicector}/>;
   }
 
   return (
@@ -39,7 +56,7 @@ const Home = () => {
             Collections
             <span className="ml-2 rounded-full py-[2px] bg-success p-2">8</span>
           </p>
-          <button className="py-[10px] px-[9px] bg-primary text-[13px] font-lg font-bold rounded-md flex items-center">
+          <button onClick={createCollectionRedicector} className="py-[10px] px-[9px] bg-primary text-[13px] font-lg font-bold rounded-md flex items-center">
             {" "}
             <img src={addIcon} className="mr-2" /> Create Collection{" "}
           </button>
--- src/Screens/NewCollection.js
+++ src/Screens/NewCollection.js
@@ -1,22 +1,30 @@
 import React from "react";
 import Input, {Select} from "../Components/Input/Input";
 import BackArrow from "../assets/Icons/arrow.svg";
+import { useNavigate } from "react-router-dom";
 
 
 const NewCollection = () => {
+  const navigate = useNavigate();
   const onInput = (e) => {
     console.log(e.target.value);
   };
   const onInputFile = (e) => {
     console.log(e.target.value);
   };
+
+  const backButtonHnadler = (e) => {
+    e.preventDefault();
+    navigate(-1);
+  }
+
   return (
     <>
     {/* Need to create a shadow warppr later */}
       <div className="pt-4 pl-6 bg-bgPrimary border-b border-bgGrey px-4 pb-4 drop-shadow-md">
-        <a className="cursor-pointer flex gap-3 [&>img]:rotate-[90deg] [&>img]:w-[22px]">
+        <button onClick={backButtonHnadler} className="cursor-pointer flex items-center gap-3 [&>img]:rotate-[90deg] [&>img]:w-[22px]">
           <img src={BackArrow} /> <p className="text-textPrimary font-bold text-xl">Create Collection</p>
-        </a>
+        </button>
       </div>
       <div className="bg-bgPrimary bg-bgSecondary p-3 px-5 flex flex-col justify-center items-center h-[60%]">
         <div className="w-full">
