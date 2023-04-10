51f9dc586c10882f949acfa4b98c951f0a794e18
subhradwi01 <subhradwi01@gmail.com>
2023-04-11 02:51:56 +0530

    Getting all collections api integration

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -4,7 +4,7 @@ import CollectionItem from "../Components/CollectiionItem/CollectionItem";
 import SearchBox from "../Components/SearchBox/SearchBox";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-// import { getAllCollectionsWithoutTimelines } from "../api/collectionService";
+import { getAllCollectionsWithoutTimelines } from "../api/collectionService";
 import NoResult from "../Components/NoResult/NoResult";
 
 
@@ -21,9 +21,12 @@ const Home = () => {
     if(!auth.token){
       return navigate("/")
     }
-    // const getCollections = async () =>{
-    //   const data = await getAllCollectionsWithoutTimelines()
-    // }
+    const getCollections = async () =>{
+      const res = await getAllCollectionsWithoutTimelines()
+      console.log(res.data.data);
+      SetCollections(res.data.data)
+    }
+    getCollections(auth.token);
   },[])
   if (collections.length===0) {
     return <NoResult title="Add Collection" noResultName="collections" navigator={createCollectionRedicector}/>;
@@ -34,22 +37,22 @@ const Home = () => {
       <div className="py-3 bg-bgPrimary border-b border-bgGrey px-4">
         <SearchBox />
       </div>
-      <div className="px-4 py-4 bg-bgSecondary">
+      <div className="px-3 py-3 bg-bgSecondary">
         <div className="flex justify-between items-center">
           <p className="text-[18px] text-textPrimary">
             Collections
             <span className="ml-2 rounded-full py-[2px] bg-success p-2">8</span>
           </p>
-          <button onClick={createCollectionRedicector} className="py-[10px] px-[9px] bg-primary text-[13px] font-lg font-bold rounded-md flex items-center">
+          <button onClick={createCollectionRedicector} className="py-[9px] px-[9px] bg-primary text-[13px] font-lg font-bold rounded-md flex items-center">
             {" "}
             <img src={addIcon} className="mr-2" /> Create Collection{" "}
           </button>
         </div>
         <div className="mt-4">
-          <CollectionItem name="Python Automation" count="8" />
-          <CollectionItem name="Python Automation" count="8" />
-          <CollectionItem name="Python Automation" count="8" />
-          <CollectionItem name="Python Automation" count="8" />
+          {collections.map(collection => (
+            <CollectionItem name={collection.title} count={collection.timelines.length} img={collection.image} key={collection._id}/>
+          ))}
+          
         </div>
       </div>
     </div>
