5e60f4a72176fa9ec6b0d0575c1dfc3907acd9de
subhradwi01 <subhradwi01@gmail.com>
2023-04-12 00:03:57 +0530

    copy link and web funtionally added

--- src/Components/CollectiionItem/CollectionItem.js
+++ src/Components/CollectiionItem/CollectionItem.js
@@ -1,25 +1,75 @@
-import React from 'react'
-import logo from "../../assets/Logo.svg"
-import CopyIcon from "../../assets/Icons/copy.svg"
-import AddIcon from "../../assets/Icons/add.svg"
-import ShareIcon from "../../assets/Icons/arrow-share.svg"
-const CollectionItem = ({image,name,count,copyLinkHandler,shareLinkHandler,addHandler}) => {
+import React, { useRef, useState } from "react";
+import logo from "../../assets/Logo.svg";
+import CopyIcon from "../../assets/Icons/copy.svg";
+import AddIcon from "../../assets/Icons/add.svg";
+import ShareIcon from "../../assets/Icons/arrow-share.svg";
+import { Link } from "react-router-dom";
+import Tooltip from "../Tooltip/Tooltip";
+import Loader from "../Loader/Loader";
+import approve from "../../assets/Icons/approve.svg"
+
+const CollectionItem = ({
+  image,
+  name,
+  count,
+  copyLinkHandler,
+  addHandler,
+  id,
+}) => {
+  const [copyText,setCopyText] = useState("Copy Link");
+  const copyImageRef = useRef();
+  const onCopy = () => {
+    console.log(id);
+    setCopyText("Copied")
+    if(copyImageRef) copyImageRef.current.src=approve;
+    copyLinkHandler(id);
+    setTimeout(()=>{
+      copyImageRef.current.src=CopyIcon
+      setCopyText("Copy Link");
+
+    },2500);
+  };
+
+
+
   return (
-    <div className='bg-bgPrimary rounded-md border border-secodary  p-2 flex justify-between mb-2'>
-      <div className='flex'>
-        <img src={image || logo}/>
-        <div className='flex flex-col ml-4 '>
-          <p className='text-[17px] text-textPrimary font-bold'>{name}</p>
-          <p className='text-textPrimary text-xs'>{count}{" "}Bookmarks</p>
+    <div className="bg-bgPrimary rounded-md border border-secodary  p-2 flex justify-between mb-2">
+      <div className="flex">
+        <img src={image || logo} />
+        <div className="flex flex-col ml-4 ">
+          <p className="text-[17px] text-textPrimary font-bold">{name}</p>
+          <p className="text-textPrimary text-xs">{count} Bookmarks</p>
         </div>
       </div>
-      <div className='flex gap-2'>
-        <button className='bg-textLight rounded-full py-2 px-[8px] flex justify-center items-center' onClick={copyLinkHandler}><img className='w-[23px]' src={CopyIcon}/></button>
-        <button className='bg-textLight rounded-full py-2 px-[8px] flex justify-center items-center' onClick={shareLinkHandler}><img src={ShareIcon} className='w-[23px]'  /></button>
-        <button className='bg-primary rounded-full py-2 px-[8px] flex justify-center items-center' ocClick={addHandler}><img src={AddIcon} className='w-[23px]'/></button>
+      <div className="flex gap-2">
+        <Tooltip name={copyText}>
+          <button
+            className="bg-textLight rounded-full py-2 px-[8px] flex justify-center items-center"
+            onClick={onCopy}
+          >
+            <img src={CopyIcon} ref={copyImageRef} className="w-[23px]" />
+          </button>
+        </Tooltip>
+        <Tooltip name="Open Collection">
+          <Link
+            className="bg-textLight rounded-full py-2 px-[8px] flex justify-center items-center"
+            to={`http://localhost:3000/collections/${id}`}
+            target="_blank"
+          >
+            <img src={ShareIcon} className="w-[23px]" />
+          </Link>
+        </Tooltip>
+        <Tooltip name="Add bookmark">
+          <button
+            className="bg-primary rounded-full py-2 px-[8px] flex justify-center items-center"
+            ocClick={addHandler}
+          >
+            <img src={AddIcon} className="w-[23px]" />
+          </button>
+        </Tooltip>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CollectionItem
+export default CollectionItem;
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -6,30 +6,44 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { getAllCollectionsWithoutTimelines } from "../api/collectionService";
 import NoResult from "../Components/NoResult/NoResult";
-
+import Loader from "../Components/Loader/Loader";
 
 const Home = () => {
-  const [empty, setEmpty] = useState(false);
-  const [collections,SetCollections] = useState([]);
-  const auth = useSelector(state=>state.auth);
+  const [collections, SetCollections] = useState([]);
+  const [loading, setLoadeing] = useState(false);
+  const auth = useSelector((state) => state.auth);
   const navigate = useNavigate();
-  const createCollectionRedicector =  () => {
+  const createCollectionRedicector = () => {
     navigate("/new-collection");
-  }
+  };
 
-  useEffect(()=>{
-    if(!auth.token){
-      return navigate("/")
+  useEffect(() => {
+    if (!auth.token) {
+      return navigate("/");
     }
-    const getCollections = async () =>{
-      const res = await getAllCollectionsWithoutTimelines()
+    setLoadeing(true);
+    const getCollections = async () => {
+      const res = await getAllCollectionsWithoutTimelines();
       console.log(res.data.data);
-      SetCollections(res.data.data)
-    }
+      SetCollections(res.data.data);
+      setLoadeing(false)
+    };
     getCollections(auth.token);
-  },[])
-  if (collections.length===0) {
-    return <NoResult title="Add Collection" noResultName="collections" navigator={createCollectionRedicector}/>;
+  }, []);
+
+  const handleCopy = (collectionId ) => {
+    console.log(collectionId)
+    navigator.clipboard.writeText(`http://localhost:3000/collections/${collectionId}`);
+  };
+
+  if (!loading && collections.length === 0) {
+    return (
+      <NoResult
+        title="Add Collection"
+        noResultName="collections"
+        navigator={createCollectionRedicector}
+      />
+    );
   }
 
   return (
@@ -37,24 +51,37 @@ const Home = () => {
       <div className="py-3 bg-bgPrimary border-b border-bgGrey px-4">
         <SearchBox />
       </div>
-      <div className="px-3 py-3 bg-bgSecondary">
+      {!loading? <div className="px-3 py-3 bg-bgSecondary">
         <div className="flex justify-between items-center">
           <p className="text-[18px] text-textPrimary">
             Collections
             <span className="ml-2 rounded-full py-[2px] bg-success p-2">8</span>
           </p>
-          <button onClick={createCollectionRedicector} className="py-[9px] px-[9px] bg-primary text-[13px] font-lg font-bold rounded-md flex items-center">
+          <button
+            onClick={createCollectionRedicector}
+            className="py-[9px] px-[9px] bg-primary text-[13px] font-lg font-bold rounded-md flex items-center"
+          >
             {" "}
             <img src={addIcon} className="mr-2" /> Create Collection{" "}
           </button>
         </div>
         <div className="mt-4">
-          {collections.map(collection => (
-            <CollectionItem name={collection.title} count={collection.timelines.length} img={collection.image} key={collection._id}/>
+          {collections.map((collection) => (
+            <CollectionItem
+              name={collection.title}
+              count={collection.timelines.length}
+              img={collection.image}
+              key={collection._id}
+              id={collection._id}
+              copyLinkHandler={handleCopy}
+            />
           ))}
-          
         </div>
+      </div> : 
+      <div className="flex w-full h-[70vh] justify-center items-center">
+        <Loader/>
       </div>
+      }
     </div>
   );
 };
