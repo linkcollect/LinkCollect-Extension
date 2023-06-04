f7cf37f666ded42ab40dd6f392194b8bb9a72588
Harsh8055 <askwhyharsh@gmail.com>
2023-06-04 15:16:19 +0530

    text changes

--- src/Components/Offline/Offline.js
+++ src/Components/Offline/Offline.js
@@ -7,8 +7,8 @@ const Offline = () => {
       <img src={serverDown} className="mr-2" />
     <div className="flex flex-col items-center">
       <h4 className='text-textPrimary font-bold text-[20px] mb-5'>Looks like Something went wrong!</h4>
-       <p className='text-textPrimary text-[16px] w-[60%] text-center'>We are on the way to fix it</p>
-       <p className='text-textPrimary text-[16px] w-[80%] text-center'>You may refresh or try again later</p>
+       <p className='text-textPrimary text-[16px] w-[80%] mb-3 text-center'>You may check your network once, if network is fine the our server might be offline... 🤕 </p>
+       <p className='text-textPrimary text-[16px] w-[80%] text-center'>But hey, your bookmarks are safe with us 👍</p>
     </div>
   </div>
   )
