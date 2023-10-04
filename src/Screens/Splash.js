961b387de8a6d16fbfc93b30d1aef51e6337b828
Harsh8055 <harshsingh.eth@gmail.com>
2023-10-05 01:29:50 +0530

    fix get started

--- src/Screens/Splash.js
+++ src/Screens/Splash.js
@@ -20,13 +20,13 @@ const Splash = () => {
       <div className='relative flex flex-col justify-center items-center'>
         <img className='w-[85%]' src={splashLogo}/>
         <div className='conatiner text-center absolute top-[200px]'>
-            <p className='text-[22px] text-textPrimary font-bold mb-2 mt-2'>Stay organized, save your <br/> web links</p>
+            <p className='text-[22px] text-textPrimary font-bold mb-2 mt-2'>Stay organized, save your web links</p>
             <p className='text-textPrimary font-light text-[14px] flex flex-col w-full'>
             linkcollect is the simplest way to save & share
-            </p>
-            <p className='text-textPrimary font-light text-[14px] flex flex-col w-full'>
             web links from anywhere to anyone
             </p>
+            {/* <p className='text-textPrimary font-light text-[14px] flex flex-col w-full'> */}
+            {/* </p> */}
          <div className='absolute top-[100px] left-[0px] flex justify-center w-full'> 
          <button onClick={startLoginHanlder} target='_blank' className='py-[10px] px-[36px] mt-[50px] bg-primary text-[14px] font-medium rounded-md'>Get Started</button>
          </div>
